72a99ba2208495fa054ce80168e7f36c1aa1d4c6
Henrique <henriqueestefanelli@gmail.com>
2025-02-27 19:44:46 -0300

    Clicker coop V2.6

--- server.js
+++ server.js
@@ -63,6 +63,4 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-server.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+module.exports = server;
